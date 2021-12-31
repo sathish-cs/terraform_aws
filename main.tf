bf21b49e0171c82215299bf732bff4f2d5f1589b
sathish-cs <sathishtech88@gmail.com>
2021-12-31 23:38:16 +0530

    Updated main config

--- main.tf
+++ main.tf
@@ -11,3 +11,12 @@ module "ec2" {
   sg_private =  ""
   sg_public = ""
 }
+
+module "alb" {
+  vpc = module.vpc.vpc
+  ec2_id = module.ec2.webservers_id
+  public_subnets = module.vpc.public_subnet
+  private_subnets = module.vpc.private_subnet
+  //lb_sg = module.ec2.lb_sg
+  source = "./modules/alb"
+}
