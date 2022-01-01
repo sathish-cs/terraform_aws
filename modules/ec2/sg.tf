11f6fdcce0de418bb7b3c46ebd4fc82c31ff6f5c
sathish-cs <sathishtech88@gmail.com>
2022-01-01 22:36:28 +0530

    added alb sg

--- modules/ec2/sg.tf
+++ modules/ec2/sg.tf
@@ -29,3 +29,18 @@
   # Egress Rules
   egress_rules = ["all-all"]
 }
+
+module "loadbalancer-sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.7.0"
+  
+  name = "loadbalancer-sg"
+  vpc_id = var.vpc
+
+  # Ingress Rules 
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  # Egress Rules
+  egress_rules = ["all-all"]
+}
