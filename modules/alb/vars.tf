83998e03fd222b1701f9069b8ec58ac5c38454e7
sathish-cs <sathishtech88@gmail.com>
2022-01-01 22:35:57 +0530

    Updated alb config

--- modules/alb/vars.tf
+++
@@ -1,6 +0,0 @@
-variable "vpc" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
-
-variable "lb_sg" {}
-variable "ec2_id" {}
--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -52,3 +52,59 @@ resource "tls_private_key" "ssh-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+#############
+
+# Terraform AWS Application Load Balancer (ALB)
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "5.16.0"
+
+  name = "demo-alb"
+  load_balancer_type = "application"
+  vpc_id = var.vpc
+  subnets = var.alb_subnets
+  security_groups = [module.loadbalancer-sg.security_group_id]
+  # Listeners
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0 # App1 TG associated to this listener
+    }
+  ]  
+  # Target Groups
+  target_groups = [
+    # App1 Target Group - TG Index = 0
+    {
+      name_prefix          = "app1-"
+      backend_protocol     = "HTTP"
+      backend_port         = 80
+      target_type          = "instance"
+      deregistration_delay = 10
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/app1/index.html"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+      protocol_version = "HTTP1"
+      # App1 Target Group - Targets
+      targets = {
+        webserver1 = {
+          target_id = module.ec2_private.id[0]
+          port      = 80
+        },
+        webserver2 = {
+          target_id = module.ec2_private.id[1]
+          port      = 80
+        }
+      }
+    }  
+  ]
+}
